3d830e802cba62eb71cdae227e0d45ae9b1fdfe5
Matias <matiaslalanne96@gmail.com>
2026-02-10 16:09:38 -0300

    clase 90

--- 04-blackjack-vite/src/blackjack/index.js
+++ 04-blackjack-vite/src/blackjack/index.js
@@ -1,8 +1,7 @@
 import _ from 'underscore';
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+import { crearDeck } from './usecases/crear-deck.js';
+
+
 
 /**
  * 2C = Two of Clubs
@@ -29,24 +28,7 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 // Esta función crea un nuevo deck
-const crearDeck = () => {
 
-    for( let i = 2; i <= 10; i++ ) {
-        for( let tipo of tipos ) {
-            deck.push( i + tipo);
-        }
-    }
-
-    for( let tipo of tipos ) {
-        for( let esp of especiales ) {
-            deck.push( esp + tipo);
-        }
-    }
-    // console.log( deck );
-    deck = _.shuffle( deck );
-    console.log( deck );
-    return deck;
-}
 
 crearDeck();
 
---
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,19 @@
+import _ from 'underscore';
+
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tipos ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
